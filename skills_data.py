3e80823382437ff569405c0c9c4579b7790c2c96
TrinushaUppalapati <trinushauppalapati@gmail.com>
2026-06-17 14:48:31 +0530

    Add job roles with required skills to skills_data.py

---
+++ skills_data.py
@@ -0,0 +1,35 @@
+job_roles = {
+    "Machine Learning Engineer": [
+        "Python",
+        "NumPy",
+        "Pandas",
+        "Scikit-learn",
+        "Statistics",
+        "Git"
+    ],
+
+    "Data Scientist": [
+        "Python",
+        "SQL",
+        "Statistics",
+        "Machine Learning",
+        "Data Visualization"
+    ],
+
+    "AI Engineer": [
+        "Python",
+        "Machine Learning",
+        "Deep Learning",
+        "TensorFlow",
+        "PyTorch",
+        "Git"
+    ],
+
+    "Data Analyst": [
+        "Excel",
+        "SQL",
+        "Python",
+        "Power BI",
+        "Data Visualization"
+    ]
+}
